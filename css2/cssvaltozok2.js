27b71c67a3b610dab1a67e6d1c7b27b174ecfd04
sz3papdav <sz3papdav@vasvari.org>
2023-09-28 12:01:13 +0200

    igen
    igen

---
+++ css2/cssvaltozok2.js
@@ -0,0 +1,14 @@
+ 
+ function swit() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ } 
+
+function myFunction() {
+   let szam = document.getElementById("szam").value;
+   let col = document.getElementById("col").value;
+   console.log(szam);
+   console.log(col);
+   let hely = "szin" + szam;
+   console.log(hely);
+}
